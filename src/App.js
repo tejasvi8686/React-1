0f9dc754e4b1d83266b1fba2a0eb1fc326db3165
tejasvi8686 <tejasviraj8686@gmail.com>
2023-05-16 00:40:07 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-import './App.css';
 
-function App() {
+import Video from './Components/Video.js'
 
-  let name  = 'react app'
-  let className = 'App-header';
-  console.log('App')
+
+function App (){
   return (
-    <div className="">
-      <div className={className}>
-         {name}
-         <Demo></Demo>
-      </div>
+    <div>
+    <div>Hello</div>
+    <Video></Video>
     </div>
-  );
-}
-function Demo() {
-  console.log('Demo')
-  return (
-    <div className="App">
-      <div className='App-header'>
-         Demo
-      </div>
-    </div>
-  );
-}
+  
+  )
+    
+  
 
 
+}
 
 export default App;
---
+++ src/Components/Video.js
@@ -0,0 +1,21 @@
+import './Video.css'
+
+function Video(){
+    return(
+        <div>
+        <div>Video</div>
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_1280.png" alt="person" height=
+            {200} width={200}></img>
+            <div> React Js Tutorial - Components</div>
+        
+        </div>
+
+        </div>
+        
+    )
+
+}
+
+
+export default Video;
